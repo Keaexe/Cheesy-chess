179f94a58c71bb93b69b02b4e897c84081eefb41
Keaexe <87970572+Keaexe@users.noreply.github.com>
2026-09-16 13:44:24 +0200

    fix: bug from last commit where pawn could not move

--- main.js
+++ main.js
@@ -268,7 +268,7 @@ function pawnCase(movement) {
     if (delta.row !== (gameState.toPlay === 0 ? -2 : 2)) {
       return false;
     }
-    if (board[movement.row][board.movement.column].innerHTML !== '') {
+    if (board[movement.to.row][movement.to.column].innerHTML !== '') {
       return false;
     }
     // movement is legal, now looking for en passant
